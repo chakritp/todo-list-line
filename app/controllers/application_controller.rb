a39db8f39ea30aa012800277edef8393e0569804
Chakrit Prasatwattana <chakritp888@gmail.com>
2018-06-28 20:19:46 +0700

    Add verify_signature action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
+  
+  private
+
+  def verify_signature
+    http_request_body = request.raw_post
+    hash = OpenSSL::HMAC::digest(OpenSSL::Digest::SHA256.new, ENV["LINE_CHANNEL_SECRET"], http_request_body)
+    signature = Base64.strict_encode64(hash)
+
+    unless signature == request.headers["HTTP_X_LINE_SIGNATURE"]
+      render json: {
+        text: "Bad Request",
+        status: 400
+      }
+    end
+  end
 end
