71034bf1745c7b1bf3cc40f0e527e26e5c6b88bd
Chakrit Prasatwattana <chakritp888@gmail.com>
2018-06-29 12:59:48 +0700

    Change database from sqlite to pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180628234153) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "todo_items", force: :cascade do |t|
     t.string "name"
     t.datetime "due_date"
@@ -19,7 +22,7 @@ ActiveRecord::Schema.define(version: 20180628234153) do
     t.boolean "is_completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.bigint "user_id"
     t.integer "position"
     t.index ["user_id"], name: "index_todo_items_on_user_id"
   end
@@ -31,4 +34,5 @@ ActiveRecord::Schema.define(version: 20180628234153) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "todo_items", "users"
 end
