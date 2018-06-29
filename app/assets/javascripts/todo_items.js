e0a6814d2fdf38bad097e67214c073163ad56905
Chakrit Prasatwattana <chakritp888@gmail.com>
2018-06-29 08:04:06 +0700

    Handle sorting of important and unimportant tasks

--- app/assets/javascripts/todo_items.js
+++ app/assets/javascripts/todo_items.js
@@ -1,11 +1,38 @@
+function isValidPlacement() {
+  //check that all important tasks are on top
+  var endOfImportantGroup = false
+  var isValid = true
+  var $items = $('#todos').find('.list-group-item')
+  
+  $.each($items, function(index, item) {
+    // console.log(item)
+    // console.log($(item))
+    if (!$(item).hasClass('important') && !endOfImportantGroup) {
+      endOfImportantGroup = true
+    } else if ($(item).hasClass('important') && endOfImportantGroup) {
+      isValid = false
+    }
+  })
+  return isValid
+}
+
 document.addEventListener("turbolinks:load", function() {
   $("#todos").sortable({
     update: function(e, ui) {
-      $.ajax({
-        url: $(this).data('url'),
-        type: 'PATCH',
-        data: $(this).sortable('serialize')
-      })
+      console.log("isValidPlacement", isValidPlacement())
+      if (isValidPlacement()) {
+        $.ajax({
+          url: $(this).data('url'),
+          type: 'PATCH',
+          data: $(this).sortable('serialize')
+        })
+      }
+    },
+    stop: function(e, ui){
+      // check if importance clashes
+      if(!isValidPlacement()) {
+        $("#todos").sortable('cancel')
+      }
     }
   })
 })
